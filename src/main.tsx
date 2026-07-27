1d0ca72741ab01aa4ef9bb569d8385723daaf0bf
1264585648 <77035161+1264585648@users.noreply.github.com>
2026-07-27 15:50:23 +0800

    Load formats page calibration styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './home-calibration.css'
 import './converter-calibration.css'
+import './formats-calibration.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
