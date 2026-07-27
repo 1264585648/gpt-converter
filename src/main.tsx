402e11358a0647b12e37dea75a468ad7407ef4d7
1264585648 <77035161+1264585648@users.noreply.github.com>
2026-07-27 15:48:06 +0800

    Load converter page calibration styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './home-calibration.css'
+import './converter-calibration.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
