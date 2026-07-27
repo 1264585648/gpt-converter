5eb556ca2e8519fee8260c487280d8fc64e8fcc5
1264585648 <77035161+1264585648@users.noreply.github.com>
2026-07-27 18:10:23 +0800

    refactor: route formats to shared-data workspace

--- src/components/ClientApp.tsx
+++ src/components/ClientApp.tsx
@@ -1,10 +1,16 @@
 import App from '../App'
 import QaSafetyBoundary from '../QaSafetyBoundary'
+import FormatsWorkspace from './FormatsWorkspace'
+
+function isFormatsRoute() {
+  if (typeof window === 'undefined') return false
+  return (window.location.pathname.replace(/\/+$/, '') || '/') === '/formats'
+}
 
 export default function ClientApp() {
   return (
     <QaSafetyBoundary>
-      <App />
+      {isFormatsRoute() ? <FormatsWorkspace /> : <App />}
     </QaSafetyBoundary>
   )
 }
