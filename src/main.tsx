cb9187e5027bac343840a4d2163585b5d3a93381
1264585648 <77035161+1264585648@users.noreply.github.com>
2026-07-27 15:58:04 +0800

    Load interaction QA styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './home-calibration.css'
 import './converter-calibration.css'
 import './formats-calibration.css'
 import './compare-calibration.css'
+import './qa-fixes.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
